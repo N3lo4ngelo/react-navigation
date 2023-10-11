1a4cb73bd78623604b25bcc26e139dbdf08e512f
Thiago Santos de Almeida <thisantosareis@gmail.com>
2023-10-11 11:56:48 -0300

    screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './src/screens/home';
-import DetailsScreen from './src/screens/details';
-import SettingsScreen from './src/screens/settings';
+
+import HomeScreen from '../screens/home';
+import DetailsScreen from '../screens/details';
+import SettingsScreen from '../screens/settings';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Inicio" component={HomeScreen} />
         <Stack.Screen name="Detalhes" component={DetailsScreen} />
         <Stack.Screen name="Configurações" component={SettingsScreen} />
